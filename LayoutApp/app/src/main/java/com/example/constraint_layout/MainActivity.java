b1c0551ce735f8d06ff0170bfb50835e368acf78
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-08-09 01:13:31 +0530

    Added Explicit Intent In this App to Go on a Second Activity page.

--- LayoutApp/app/src/main/java/com/example/constraint_layout/MainActivity.java
+++ LayoutApp/app/src/main/java/com/example/constraint_layout/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.constraint_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button myBtn = findViewById(R.id.btn);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSecondActivity();
+            }
+        });
+
+
         // Initializing UI Elements and Variables here
         Toast.makeText(this,
                 "onCreate() is called ",
                 Toast.LENGTH_SHORT).show();
     }
+
+    public void goToSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+
 
     @Override
     protected void onStart() {
