8b286de3bebff76334f74a37a20d574872cf02ba
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-08-10 23:35:56 +0530

    added Intent

--- LuckyNumber/app/src/main/java/com/example/luckynumber/MainActivity.java
+++ LuckyNumber/app/src/main/java/com/example/luckynumber/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.luckynumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.input_name);
         luckBtn = findViewById(R.id.luck_btn);
 
+        luckBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSecondPage();
+            }
+        });
     }
+        public void goToSecondPage(){
+            Intent intent = new Intent(this, SecondPage.class);
+            startActivity(intent);
+        }
 }
