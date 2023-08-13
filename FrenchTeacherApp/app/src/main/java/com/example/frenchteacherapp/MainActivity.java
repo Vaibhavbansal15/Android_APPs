d54004d0f8c4fa82446e8c646578d43dc19f6dab
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-08-13 22:01:24 +0530

    Designed the Layout of French Teacher App

--- FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.java
+++ FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.frenchteacherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcomeText = findViewById(R.id.textView);
+        Button black = findViewById(R.id.blackBtn);
+        Button green = findViewById(R.id.greenBtn);
+        Button purple = findViewById(R.id.purpleBtn);
+        Button red = findViewById(R.id.redBtn);
+        Button yellow = findViewById(R.id.yellowBtn);
     }
 }
