51a7b1f3c393cceda714fc3c2a75bcc45a1ad09b
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-08-15 13:11:19 +0530

    Added TimePicker, DatePicker and ProgressBar widgets

--- WidgetsApp2/app/src/main/java/com/example/widgetsapp2/MainActivity.java
+++ WidgetsApp2/app/src/main/java/com/example/widgetsapp2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.widgetsapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -22,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,
                         "Hour: "+hourOfDay + " minute: "+minute,
                         Toast.LENGTH_LONG).show();
+            }
+        });
+
+        DatePicker datePicker = findViewById(R.id.datePicker);
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Day = "Day "+datePicker.getDayOfMonth();
+                String Month = "Month "+(datePicker.getMonth()+1);
+                String Year = "Year "+datePicker.getYear();
+
+                Toast.makeText(MainActivity.this,
+                        Day+" "+Month+" "+Year,
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
