5c4a5a696bb8b5fce48bc94277ebef48788be14e
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-08-17 23:05:07 +0530

    Designed the Custom ListView and Model Class of PlanetApp

---
+++ PlanetsApp/app/src/main/java/com/example/planetsapp/MyCustomAdapter.java
@@ -0,0 +1,9 @@
+package com.example.planetsapp;
+
+import android.widget.ArrayAdapter;
+
+public class MyCustomAdapter extends ArrayAdapter<Planet> {
+    // Using Custom Layout  --> MyCustomAdapter
+    // Using Custom Objects --> extends ArrayAdatper<Planet>
+    
+}
