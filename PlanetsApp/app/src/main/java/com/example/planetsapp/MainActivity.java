65c8446946429da8f08ce97fd12840e5fec7418b
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-08-18 00:17:47 +0530

    Completed PlanetsApp

--- PlanetsApp/app/src/main/java/com/example/planetsapp/MainActivity.java
+++ PlanetsApp/app/src/main/java/com/example/planetsapp/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.planetsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
+    ArrayList<Planet> planetArrayList;
+    private static MyCustomAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1- AdapterView: listView
+        listView = findViewById(R.id.planetList);
+
+        // 2- DataSource: ArrayList<Planet>
+        planetArrayList = new ArrayList<>();
+
+        Planet planet1 = new Planet("Mercury","0 Moon",R.drawable.mercury);
+        Planet planet2 = new Planet("Venus","0 Moon",R.drawable.venus);
+        Planet planet3 = new Planet("Earth","1 Moon",R.drawable.earth);
+        Planet planet4 = new Planet("Mars","2 Moon",R.drawable.mars);
+        Planet planet5 = new Planet("Jupiter","79 Moon",R.drawable.jupiter);
+        Planet planet6 = new Planet("Saturn","83 Moon",R.drawable.saturn);
+        Planet planet7 = new Planet("Uranus","27 Moon",R.drawable.uranus);
+        Planet planet8 = new Planet("Neptune","14 Moon",R.drawable.neptune);
+
+        planetArrayList.add(planet1);
+        planetArrayList.add(planet2);
+        planetArrayList.add(planet3);
+        planetArrayList.add(planet4);
+        planetArrayList.add(planet5);
+        planetArrayList.add(planet6);
+        planetArrayList.add(planet7);
+        planetArrayList.add(planet8);
+
+        // Adapter:
+        adapter = new MyCustomAdapter(planetArrayList, getApplicationContext());
+        listView.setAdapter(adapter);
+
+        // Handling Click events
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        "Planet Name: "+adapter.getItem(position).getPlanetName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- PlanetsApp/app/src/main/java/com/example/planetsapp/MyCustomAdapter.java
+++ PlanetsApp/app/src/main/java/com/example/planetsapp/MyCustomAdapter.java
@@ -1,9 +1,80 @@
 package com.example.planetsapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MyCustomAdapter extends ArrayAdapter<Planet> {
     // Using Custom Layout  --> MyCustomAdapter
-    // Using Custom Objects --> extends ArrayAdatper<Planet>
-    
+    // Using Custom Objects --> extends ArrayAdapter<Planet>
+    private ArrayList<Planet> planetsArrayList;
+    Context context;
+
+    public MyCustomAdapter(ArrayList<Planet> planetsArrayList, Context context) {
+        super(context, R.layout.item_list_layout, planetsArrayList);
+        this.planetsArrayList = planetsArrayList;
+        this.context = context;
+    }
+
+    // Holder Class
+    private static class MyViewHolder{
+        TextView planetName;
+        TextView moonCount;
+        ImageView planetImg;
+
+    }
+
+    // getView()
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // 1- Get the Planet object for the current position
+        Planet planets = getItem(position);
+
+        //2- Inflate layout
+        MyViewHolder myViewHolder;
+        final View result;
+        if(convertView == null) {
+            myViewHolder = new MyViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(
+                    R.layout.item_list_layout,
+                    parent,
+                    false
+            );
+
+            // Finding Views
+            myViewHolder.planetName = (TextView) convertView.findViewById(R.id.planet_name);
+            myViewHolder.planetImg = (ImageView) convertView.findViewById(R.id.planet_img);
+            myViewHolder.moonCount = (TextView) convertView.findViewById(R.id.moons);
+
+            result = convertView;
+
+            convertView.setTag(myViewHolder);
+        }else {
+            // the view is recycled
+            myViewHolder = (MyViewHolder) convertView.getTag();
+            result = convertView;
+
+            // getting the data from the model class
+            myViewHolder.planetName.setText(planets.getPlanetName());
+            myViewHolder.moonCount.setText(planets.getMoonCount());
+            myViewHolder.planetImg.setImageResource(planets.getPlanetImage());
+
+        }
+        return result;
+    }
 }
