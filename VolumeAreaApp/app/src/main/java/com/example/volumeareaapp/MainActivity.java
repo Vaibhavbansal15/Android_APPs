6e20fe7588951247cc04e7431d99c599a20c4cd4
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-08-19 20:56:17 +0530

    Completed the Layout left with calculation

--- VolumeAreaApp/app/src/main/java/com/example/volumeareaapp/MainActivity.java
+++ VolumeAreaApp/app/src/main/java/com/example/volumeareaapp/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.volumeareaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    // 1- AdapterView: GridView
+    GridView gridView;
+
+    // 2- Data Source: ArrayList<Shape>
+    ArrayList<Shape> shapeArrayList;
+
+    // 3- Adapter: MyCustomAdapter
+    MyCustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridView);
+        shapeArrayList = new ArrayList<>();
+
+        Shape s1 = new Shape(R.drawable.sphere, "Sphere");
+        Shape s2 = new Shape(R.drawable.cylinder, "Cylinder");
+        Shape s3 = new Shape(R.drawable.cube, "Cube");
+        Shape s4 = new Shape(R.drawable.triangular, "Prism");
+
+        shapeArrayList.add(s1);
+        shapeArrayList.add(s2);
+        shapeArrayList.add(s3);
+        shapeArrayList.add(s4);
+
+        adapter = new MyCustomAdapter(shapeArrayList, getApplicationContext());
+        gridView.setAdapter(adapter);
+        gridView.setNumColumns(2);
+
     }
 }
--- VolumeAreaApp/app/src/main/java/com/example/volumeareaapp/MyCustomAdapter.java
+++ VolumeAreaApp/app/src/main/java/com/example/volumeareaapp/MyCustomAdapter.java
@@ -1,13 +1,20 @@
 package com.example.volumeareaapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.util.ArrayList;
 
-//This class is responsible for creating the view for each item and binding data to it
+//This class is responsible for creating the view for each item
+// and binding data to it
 public class MyCustomAdapter extends ArrayAdapter<Shape> {
     private ArrayList<Shape> shapesArrayList;
     Context context;
@@ -18,8 +25,47 @@ public class MyCustomAdapter extends ArrayAdapter<Shape> {
         this.context = context;
     }
 
+//    ViewHolder
     private static class MyViewHolder{
         TextView shapeName;
         ImageView shapeImg;
+    }
+
+//    GetView() : used to create and return a view for a specific item in Grid.
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Get the Shape object for the current position
+        Shape shapes = getItem(position);
+
+        // Inflating Layout:
+        MyViewHolder myViewHolder;
+        if(convertView == null){
+            // No view went off the screen --> Create a new View
+            myViewHolder = new MyViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(
+                    R.layout.grid_item_layout,
+                    parent,
+                    false);
+
+            // Finding the views
+            myViewHolder.shapeName = (TextView) convertView.findViewById(R.id.textView);
+            myViewHolder.shapeImg = (ImageView) convertView.findViewById(R.id.imageView);
+
+            convertView.setTag(myViewHolder);
+
+        }else{
+        // a view went off the screen --> reuse it
+            myViewHolder =(MyViewHolder) convertView.getTag();
+        }
+
+        // Getting the Data from the model class
+        myViewHolder.shapeName.setText(shapes.getShapeName());
+        myViewHolder.shapeImg.setImageResource(shapes.getShapeImg());
+
+        return convertView;
     }
 }
