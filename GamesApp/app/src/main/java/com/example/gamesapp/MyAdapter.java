927d4115d30c86c3856ce8055601b8cb18f42ad2
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-09-05 21:25:50 +0600

    created Model class of GamesApp

--- GamesApp/app/src/main/java/com/example/gamesapp/MyAdapter.java
+++ GamesApp/app/src/main/java/com/example/gamesapp/MyAdapter.java
@@ -1,12 +1,18 @@
 package com.example.gamesapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MyAdapter extends RecyclerView.Adapter<Games>{
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
 
     //1- Data
     private Context context;
@@ -16,5 +22,37 @@ public class MyAdapter extends RecyclerView.Adapter<Games>{
     public MyAdapter(Context context, ArrayList<Games> gamesList) {
         this.context = context;
         this.gamesList = gamesList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Games model = gamesList.get(position);
+        holder.gameTxt.setText(model.getGameTxt());
+        holder.gamePic.setImageResource(model.getGameImg());
+    }
+
+    @Override
+    public int getItemCount() {
+        return gamesList.size();
+    }
+
+    //3- ViewHolder class
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        private ImageView gamePic;
+        private TextView gameTxt;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            gamePic = itemView.findViewById(R.id.gameImage);
+            gameTxt = itemView.findViewById(R.id.gameTitle);
+        }
     }
 }
