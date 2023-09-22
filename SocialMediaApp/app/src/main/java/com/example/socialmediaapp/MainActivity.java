4716e07c9c40920f45ee0b69405cd78f028ba6ea
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2023-09-22 23:50:19 +0530

    Initialized a social media app

---
+++ SocialMediaApp/app/src/main/java/com/example/socialmediaapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.socialmediaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+    }
+}
---
+++ SocialMediaApp/app/src/main/java/com/example/socialmediaapp/PostModel.java
@@ -0,0 +1,4 @@
+package com.example.socialmediaapp;
+
+public class PostModel{
+}
